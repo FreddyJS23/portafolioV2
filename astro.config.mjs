1f86db47fdb5b11d99735ac684c704cf8d9aed66
Freddy Solarte <freddyjsolarte@gmail.com>
2023-09-02 13:01:02 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://freddyjs23.github.io',
+   base: '/portafoliov2',
   integrations: [tailwind({
     applyBaseStyles: false
   }), react()],
